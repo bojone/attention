13bb963c40f6b9a052848f1dc49a46c05e068d18
苏剑林 <bojone@spaces.ac.cn>
2018-01-06 05:09:03 -0600

    Update attention_keras.py

--- attention_keras.py
+++ attention_keras.py
@@ -3,6 +3,37 @@
 from keras import backend as K
 from keras.engine.topology import Layer
 
+class Position_Embedding(Layer):
+    
+    def __init__(self, size=None, mode='sum', **kwargs):
+        self.size = size #必须为偶数
+        self.mode = mode
+        super(Position_Embedding, self).__init__(**kwargs)
+        
+    def call(self, x):
+        if (self.size == None) or (self.mode == 'sum'):
+            self.size = int(x.shape[-1])
+        batch_size,seq_len = K.shape(x)[0],K.shape(x)[1]
+        position_j = 1. / K.pow(10000., \
+                                 2 * K.arange(self.size / 2, dtype='float32' \
+                               ) / self.size)
+        position_j = K.expand_dims(position_j, 0)
+        position_i = K.cumsum(K.ones_like(x[:,:,0]))-1 #K.arange不支持变长，只好用这种方法生成
+        position_i = K.expand_dims(position_i, 2)
+        position_ij = K.dot(position_i, position_j)
+        position_ij = K.concatenate([K.cos(position_ij), K.sin(position_ij)], 2)
+        if self.mode == 'sum':
+            return position_ij + x
+        elif self.mode == 'concat':
+            return K.concatenate([position_ij, x], 2)
+        
+    def compute_output_shape(self, input_shape):
+        if self.mode == 'sum':
+            return input_shape
+        elif self.mode == 'concat':
+            return (input_shape[0], input_shape[1], input_shape[2]+self.size)
+
+
 class Attention(Layer):
 
     def __init__(self, nb_head, size_per_head, **kwargs):
